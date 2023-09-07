657ac3c8136a14db6a56ba90cdaa11302d490768
08/22/23 <gabriel.fernandez@saintandrews.net>
2023-09-06 23:21:49 -0400

    10/06/23 - After school v2

--- src/main/java/com/example/githubgameproject/HelloController.java
+++ src/main/java/com/example/githubgameproject/HelloController.java
@@ -4,11 +4,16 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollBar;
+
+import java.util.EventObject;
+
 public class HelloController {
     public static int rand;
     public static int score;
     public static int roundsPlayed;
+    public static double accuracy;
     public static int wrong;
+    @FXML private Button buttonSubmit;
     @FXML private Label welcomeText;
     @FXML private Label defbox;
     @FXML private Button buttonA;
@@ -17,44 +22,64 @@ public class HelloController {
     @FXML private Button buttonD;
     @FXML private  Label  wrongLabel;
     @FXML private  Label correctLabel;
-    @FXML private  Label accuracyLabel;
+    @FXML public  Label accuracyLabel;
     public  void initialize() {
         if ( roundsPlayed <= 25) {
             roundsPlayed++;
             welcomeText.setText("Please click which button this  word this definition matches with:");
             defbox.setText("Definition :  ");
-            rand = (int) ((Math.random() * GameLogic.wordList.size()));
-            String def = GameLogic.defList.get(rand);
-            String word = GameLogic.wordList.get(rand);
-            String modifiedDef = def.replace(word,"________");
-            defbox.setText(modifiedDef);
-            buttonA.setText(word);
-            Shuffle();
-        }else {welcomeText.setText("Game Oover");}
+            int listSize = GameLogic.wordList.size();
+            if ( listSize > 0 ) {
+                rand = (int) (Math.random() *listSize);
+                String def = GameLogic.defList2.get(rand);
+                String word = GameLogic.wordList2.get(rand);
+                String removeWord = GameLogic.wordList.get(rand);
+                String modifiedDef = def.replace(removeWord, "________");
+                defbox.setText(modifiedDef);
+                buttonA.setText(word);
+                Shuffle();
+            }
+        }
+        welcomeText.setText("Game Over Thank You ");
+        if (accuracy < 70) {
+            accuracyLabel.setStyle("-fx-text-fill: red;");
+        } else if (accuracy >= 70 && accuracy < 79) {
+            accuracyLabel.setStyle("-fx-text-fill: orange;");
+        } else if (accuracy >= 80 && accuracy < 89) {
+        accuracyLabel.setStyle("-fx-text-fill: blue;");
+    }else if (accuracy >= 90 && accuracy <= 100) {
+            accuracyLabel.setStyle("-fx-text-fill: green;");
+        }
     }
     public void buttonClicked(ActionEvent itemClicked )
     {
-     //   GameLogic gameLogic = new GameLogic();
         Button buttonClicked= (Button) itemClicked.getSource();
         String buttonText = buttonClicked.getText();
-        String currentWord = GameLogic.wordList.get(rand);
-        if (currentWord.equals(buttonText)){
-            GameLogic.wordList.remove(rand);
-            GameLogic.defList.remove(rand);
-            ScoreCount();
+            String currentWord = GameLogic.wordList2.get(rand);
+            if (currentWord.equals(buttonText)) {
+                GameLogic.defList2.remove(rand);
+                GameLogic.wordList.remove(rand);
+                GameLogic.defList.remove(rand);
+                GameLogic.wordList2.remove(rand);
+                ScoreCount();
+                defbox.setText((GameLogic.defList.get(rand)));
+            } else {
+                defbox.setText(GameLogic.defList.get(rand));
+                GameLogic.defList2.remove(rand);
+                GameLogic.wordList.remove(rand);
+                GameLogic.defList.remove(rand);
+                GameLogic.wordList2.remove(rand);
+                ScoreWrong();
+            }
             initialize();
-        } else {
-            GameLogic.wordList.remove(rand);
-            GameLogic.defList.remove(rand);
-            ScoreWrong();
-            initialize();
-            //What to do when incorrect
-        }
-        }
+
+    }
+
     public void ScoreCount() {
         score++;
+        accuracy=((100.0*score)/ 25);
         correctLabel.setText(String.valueOf(score));
-        accuracyLabel.setText(String.valueOf(((100.0*score)/ 25)));
+        accuracyLabel.setText(String.valueOf(accuracy));
     }
     public  void ScoreWrong(){
         wrong++;
@@ -67,9 +92,9 @@ public class HelloController {
         // if ((rand2 !=(rand) && rand3 !=(rand) && rand4 !=(rand)) && (rand2 != rand3 || rand3 != rand4 || rand2 != rand4))
         // if (((rand < rand2 || rand < rand3 || rand < rand4) && (rand > rand2 || rand > rand3 || rand > rand4)) && (rand2 != rand3 || rand3 != rand4 || rand2 != rand4)) {
         if (((rand != rand2) && (rand != rand3) && (rand != rand4)) && ((rand2 != rand3) && (rand4 != rand3) && (rand2 != rand4))) {
-            buttonB.setText(GameLogic.wordList.get(rand2));
-            buttonC.setText(GameLogic.wordList.get(rand3));
-            buttonD.setText(GameLogic.wordList.get(rand4));
+            buttonB.setText(GameLogic.wordList2.get(rand2));
+            buttonC.setText(GameLogic.wordList2.get(rand3));
+            buttonD.setText(GameLogic.wordList2.get(rand4));
         } else {
             buttonB.setText(GameLogic.wordList2.get((int) ((Math.random() * GameLogic.wordList2.size()))));
             buttonC.setText(GameLogic.wordList2.get((int) ((Math.random() * GameLogic.wordList2.size()))));
@@ -77,4 +102,5 @@ public class HelloController {
         }
     }
 }
+
 
